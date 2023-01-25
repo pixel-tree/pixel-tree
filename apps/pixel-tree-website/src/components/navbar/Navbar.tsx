1018f96fe1d491e2d3358d8681412cb432945376
DanielC78 <3024465830102@ingenieria.usac.edu.gt>
2023-01-24 22:59:57 -0600

    feat(website): :zap: change map navbar items

--- apps/pixel-tree-website/src/components/navbar/Navbar.tsx
+++ apps/pixel-tree-website/src/components/navbar/Navbar.tsx
@@ -1,9 +1,16 @@
 import { navRoutes } from "../../routes/routes";
+import { Dropdown } from "../dropdown/Dropdown";
 import { LogoPixelTreeNoBg40 } from "../utils";
+
+const items = navRoutes.map(({ path, name }) => (
+  <li key={path} className="text-[#878593]">
+    <a href={path}>{name}</a>
+  </li>
+));
 
 export const Navbar = () => {
   return (
-    <nav className="h-20 flex justify-between bg-transparent px-28 py-10">
+    <nav className="h-20 flex justify-between bg-transparent px-5 md:px-40 py-6">
       <div className="">
         <h2 className="flex items-center gap-4 text-white font-medium text-2xl">
           <LogoPixelTreeNoBg40 />
@@ -11,13 +18,8 @@ export const Navbar = () => {
         </h2>
       </div>
       <div>
-        <ul className="flex gap-8">
-          {navRoutes.map(({ path, name }) => (
-            <li key={path} className="text-[#878593]">
-              <a href={path}>{name}</a>
-            </li>
-          ))}
-        </ul>
+        <ul className="md:flex gap-8 hidden">{items}</ul>
+        <Dropdown>{items}</Dropdown>
       </div>
     </nav>
   );
