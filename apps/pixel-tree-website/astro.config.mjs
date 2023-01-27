f610081f190920b0e6f0a77e38a45f7b8d1988d7
Damian <damianpeaf@gmail.com>
2023-01-27 13:10:11 -0600

    feat(website): :sparkles: sitemap added

--- apps/pixel-tree-website/astro.config.mjs
+++ apps/pixel-tree-website/astro.config.mjs
@@ -1,12 +1,24 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from 'astro/config'
 
 // https://astro.build/config
-import tailwind from "@astrojs/tailwind";
+import tailwind from '@astrojs/tailwind'
 
 // https://astro.build/config
-import react from "@astrojs/react";
+import react from '@astrojs/react'
+
+// https://astro.build/config
+import sitemap from '@astrojs/sitemap'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()]
-});
+  site: 'https://pixel-tree.netlify.app',
+  integrations: [tailwind(), react(), sitemap({
+    i18n: {
+      // TODO: Blog routes
+      defaultLocale: 'es',
+      locales: {
+        es: 'es-ES'
+      }
+    }
+  })]
+})
