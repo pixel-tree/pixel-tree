f04fb0e2d4c57c60d7e30625c626f976074c5a5e
DanielC78 <3024465830102@ingenieria.usac.edu.gt>
2023-01-29 20:05:27 -0600

    fix(website): :poop: changing hero component, footer need refactor

--- apps/pixel-tree-website/src/components/hero/Hero.tsx
+++ apps/pixel-tree-website/src/components/hero/Hero.tsx
@@ -1,23 +1,25 @@
-import { Button } from '../button/Button'
-import { Title, Subtitle } from '../text'
-import { LogoPixelTreeNoBg40 } from '../utils'
+import { Button } from "../button/Button";
+import { Title } from "../text";
+import { LogoPixelTreeNoBg40 } from "../utils";
 
 export const Hero = () => {
   return (
     <section
       className="
+        mt-40
         grid
         w-full
         grid-cols-1
         grid-rows-2
         gap-y-24
         px-7
+        md:mt-0
         md:grid-cols-2
         md:grid-rows-1
         md:px-32
     "
       style={{
-        height: 'calc(80vh - 80px)'
+        height: "calc(80vh - 80px)",
       }}
     >
       <div
@@ -28,11 +30,11 @@ export const Hero = () => {
             justify-center
             gap-5"
       >
-        <Title>Cosecha el futuro</Title>
-        <Subtitle>
+        <Title className="text-left">Cosecha el futuro</Title>
+        <p className="text-lg text-gray-500">
           Somos una empresa de desarrollo de software enfocada en la creación de
           soluciones tecnológicas para la industria.
-        </Subtitle>
+        </p>
         <Button>Conoce más</Button>
       </div>
       <div
@@ -45,5 +47,5 @@ export const Hero = () => {
         <LogoPixelTreeNoBg40 className="text-[160px]" />
       </div>
     </section>
-  )
-}
+  );
+};
---
+++ apps/pixel-tree-website/src/components/step/Step.tsx
@@ -0,0 +1,5 @@
+export const Step = () => {
+  return (
+    <div>Step</div>
+  )
+}
